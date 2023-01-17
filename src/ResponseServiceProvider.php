6b11b9eb7359633dd547841ae4fb01f7643d6779
Mesak <mesakey@gmail.com>
2023-01-17 22:17:20 +0800

    fix: add default error result

--- src/ResponseServiceProvider.php
+++ src/ResponseServiceProvider.php
@@ -15,15 +15,14 @@ class ResponseServiceProvider extends ServiceProvider
    *
    * @return void
    */
-  public function boot()
+  public function boot(): void
   {
     if ($this->app->runningInConsole()) {
       $this->publishes([
         __DIR__ . '/../config/api-response.php' => config_path('api-response.php'),
       ], 'api-response');
     }
-    static::$responseClass = config('api-response.response', static::$responseClass);
-    static::$responseSchema = config('api-response.schema', static::$responseSchema);
+    $this->defaultResponseConfig();
     $this->registerResponseMacro();
   }
 
@@ -32,12 +31,17 @@ class ResponseServiceProvider extends ServiceProvider
    *
    * @return void
    */
-  public function register()
+  public function register(): void
   {
     //註冊 config 檔案
     $this->mergeConfigFrom(__DIR__ . '/../config/api-response.php', 'api-response');
   }
 
+  public function defaultResponseConfig(): void
+  {
+    static::$responseClass = config('api-response.response', static::$responseClass);
+    static::$responseSchema = config('api-response.schema', static::$responseSchema);
+  }
   /**
    * Register the event listener for the event.
    *
@@ -46,13 +50,13 @@ class ResponseServiceProvider extends ServiceProvider
   public function registerResponseMacro(): void
   {
     Response::macro('success', function ($result = null, $status = 200) {
-      return tap(new \Mesak\LaravelApiResponse\ResponseServiceProvider::$responseClass($result), function ($response) use ($status) {
+      return tap(new ResponseServiceProvider::$responseClass($result), function ($response) use ($status) {
         $response->setStatusCode($status);
       });
     });
-    Response::macro('error', function ($exception, $status = 400) {
+    Response::macro('error', function ($exception = null, $status = 400) {
       $exception = $exception ?? new BaseException();
-      return tap(new \Mesak\LaravelApiResponse\ResponseServiceProvider::$responseClass($exception), function ($response) use ($status) {
+      return tap(new ResponseServiceProvider::$responseClass($exception), function ($response) use ($status) {
         $response->setStatusCode($status);
       });
     });
